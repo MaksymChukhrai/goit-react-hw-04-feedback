498fd796ff029cf22c5fd3d32318c21124d7f077
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2023-07-22 04:21:14 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,16 @@
-import React, { useState } from 'react';
-import Statistics from './Statistics';
-import FeedbackOptions from './FeedbackOptions';
-import Notification from './Notification';
-
-const App = () => {
-  const [feedbackCount, setFeedbackCount] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-
-  const handleFeedback = feedback => {
-    setFeedbackCount(prevCount => ({
-      ...prevCount,
-      [feedback]: prevCount[feedback] + 1,
-    }));
-  };
-
-  const { good, neutral, bad } = feedbackCount;
-  const totalFeedback = good + neutral + bad;
-  const positivePercentage =
-    totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
-
+export const App = () => {
   return (
-    <div>
-      <FeedbackOptions
-        options={['good', 'neutral', 'bad']}
-        onLeaveFeedback={handleFeedback}
-      />
-      {totalFeedback > 0 ? (
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={totalFeedback}
-          positivePercentage={positivePercentage}
-        />
-      ) : (
-        <Notification message="There is no feedback" />
-      )}
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
     </div>
   );
 };
-
-export default App;
